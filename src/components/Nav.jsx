5d4e27e579421e42dfded9ac0c4d6fe3ba0d5579
Joshua Ortega <Josh.Co.106@gmail.com>
2024-09-03 14:31:07 +0800

    Added Form Clearing

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,7 +13,7 @@ const Nav = ({ userName }) => {
         aria-label="Go to home"
       >
         <img src={logomark} alt="" height={30} />
-        <span>HomeBudget</span>
+        <span>Finance Manager</span>
       </NavLink>
       {
         userName && (
